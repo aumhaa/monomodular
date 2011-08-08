2e64927ff6227cb3d5fa1dbaef05b77b48e6034c
amounra <aumhaa@gmail.com>
2011-08-08 21:34:43 +0000

    removed pre b99 file references

--- b99e/Monomodular/__init__.py
+++
@@ -1,8 +0,0 @@
-# http://aumhaa.blogspot.com
-
-from Monomodular import Monomodular
-
-def create_instance(c_instance):
-	""" Creates and returns the MonOhmod script """
-	return Monomodular(c_instance)
-
