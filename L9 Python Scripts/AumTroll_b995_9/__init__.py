4e10c72a85b4c865b557f7c5e77427ca056c6302
amounra <aumhaa@gmail.com>
2013-04-12 20:09:06 +0000

    removed AumTroll

--- L9 Python Scripts/AumTroll_b995_9/__init__.py
+++
@@ -1,8 +0,0 @@
-# by amounra 0313 : http://www.aumhaa.com
-
-from AumTroll import AumTroll
-
-def create_instance(c_instance):
-    """ Creates and returns the Minus script """
-    return AumTroll(c_instance)
-
